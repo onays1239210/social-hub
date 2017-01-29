df754a5854a5a98f95a24bb18db2810504ea8467
onays1239210 <onays1239210@gmail.com>
2017-01-29 02:05:38 -0800

    Revert "classic socaial network"
    This reverts commit 28de9827322a167f9b1d79e2b2632b4257beb2c5.

--- archive/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//single place to change servername, username, and password
-	
-	$servername = "localhost";
-	$dbusername = "root";
-	$dbpassword = "jeff1239210";
-?>
--- php/signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_SESSION['LoggedIn'] == true){
-	session_destroy();
-	session_unset();
-	echo 'pass';
-}
-?>
